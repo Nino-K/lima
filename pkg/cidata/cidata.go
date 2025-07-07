53dcc02692ffed9b0591d75ec5395367f3eda0bb
Nino Kodabande <nkodabande@suse.com>
2025-07-07 09:55:57 -0700

    Add Port Watcher configs to install systemd
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

--- pkg/cidata/cidata.go
+++ pkg/cidata/cidata.go
@@ -145,9 +145,9 @@ func templateArgs(ctx context.Context, bootScripts bool, instDir, name string, i
 		TimeZone:   *instConfig.TimeZone,
 		Param:      instConfig.Param,
 		PortMonitor: PortMonitor{
-			Docker:     Engine{Sockets: instConfig.PortMonitors.Docker.Sockets},
-			Containerd: Engine{Sockets: instConfig.PortMonitors.Containerd.Sockets},
-			Kubernetes: Kubernetes{Configs: instConfig.PortMonitors.Kubernetes.Configs},
+			Docker:     strings.Join(instConfig.PortMonitors.Docker.Sockets, ","),
+			Containerd: strings.Join(instConfig.PortMonitors.Containerd.Sockets, ","),
+			Kubernetes: strings.Join(instConfig.PortMonitors.Kubernetes.Configs, ","),
 		},
 	}
 
